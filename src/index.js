3f41266c5694fe01622660512931504199a14879
Your NameIryna <irina.hr@gmail.com>
2023-01-01 15:25:38 -0800

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function reverse (n) {
   
 }
+
+function reversedNum(num) {
+    return (
+      parseFloat(
+        num
+          .toString()
+          .split('')
+          .reverse()
+          .join('')
+      ) * Math.sign(num)
+    ) 
+    return num;                
+  }
+
+  console.log(reversedNum(23987))
+  
+
+  function reverse_a_number(n)
+  {
+      n = n + "";
+      return n.split("").reverse().join("");
+  }
+  console.log(Number(reverse_a_number(32243)));
+
+
+
+
+let number = 123456789;
+
+// Initializing the result variable 
+let result = 0;
+
+while(number>0){
+    // Getting the rightmost digit
+    rightmost = number%10;
+    
+    result = result*10 + rightmost;
+    
+    // Removing the rightmost digit from the number
+    number = Math.floor(number/10);
+}
+
+console.log("Reverse " +result)
