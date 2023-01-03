9ac3efc55089dbf28c434b9f927970b5a293cd28
Your NameIryna <irina.hr@gmail.com>
2023-01-03 12:40:33 -0800

    fixed issues

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 module.exports = function reverse (n) {
-  
+  return reversedNum(n);
 }
 
 function reversedNum(num) {
-    return (
-      parseFloat(
-        num
-          .toString()
-          .split('')
-          .reverse()
-          .join('')
-      ) * Math.sign(num)
-    ) 
-    return num;                
-  }
-
-  console.log(reversedNum(23987))
-  
-
-  function reverse_a_number(n)
-  {
-      n = n + "";
-      return n.split("").reverse().join("");
-  }
-  console.log(Number(reverse_a_number(32243)));
-
-
-let number = 123456789;
-
-// Initializing the result variable 
-let result = 0;
-
-while(number>0){
-    // Getting the rightmost digit
-    rightmost = number%10;
-    
-    result = result*10 + rightmost;
-    
-    // Removing the rightmost digit from the number
-    number = Math.floor(number/10);
+    let numReversedStr = num.toString().split('').reverse().join('');
+    let out = parseFloat(numReversedStr);
+    return out;       
 }
-
-console.log("Reverse " +result)
